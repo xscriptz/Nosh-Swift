b5c8fc3a11913a00105a7ad480408d5bfa71ba1f
xscriptz <athallur@iu.edu>
2016-03-25 14:43:28 -0400

    Added fixed Login files

---
+++ Nosh Swift/Mainpage Files/MainpageVC.swift
@@ -0,0 +1,57 @@
+//
+//  MainpageVC.swift
+//  Nosh Swift
+//
+//  Created by advatiya prasad on 3/25/16.
+//  Copyright © 2016 advatiya prasad. All rights reserved.
+//
+
+import UIKit
+
+class MainpageVC: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.usernameLabel.text = prefs.valueForKey("USERNAME") as! NSString as String
+        }
+        
+        
+    }
+    
+    @IBAction func LogoutTapped(sender: UIButton) {
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nosh Swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Nosh Swift
+//
+//  Created by advatiya prasad on 3/25/16.
+//  Copyright © 2016 advatiya prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
