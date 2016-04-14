0f3c6b486f56e35a95524e15e0bfea3b03e67c17
xscriptz <athallur@iu.edu>
2016-04-14 10:59:36 -0400

    Successfully downloaded an Image and a JSON

--- Nosh Swift/Constants.swift
+++ Nosh Swift/Constants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 //WARNING: To run this sample correctly, you must set the following constants.
-let CognitoRegionType = AWSRegionType.Unknown  // e.g. AWSRegionType.USEast1
-let DefaultServiceRegionType = AWSRegionType.Unknown // e.g. AWSRegionType.USEast1
-let CognitoIdentityPoolId = "YourCognitoIdentityPoolId"
-let S3BucketName = "YourS3BucketName"
+let CognitoRegionType = AWSRegionType.USEast1  // e.g. AWSRegionType.USEast1
+let DefaultServiceRegionType = AWSRegionType.USEast1 // e.g. AWSRegionType.USEast1
+let CognitoIdentityPoolId = "us-east-1:764a4604-5df9-4f74-a3c5-1ce9f0cb1db3"
+let S3BucketName = "nosh-swift"
--- Nosh Swift/FileDwn/FileVC.swift
+++ Nosh Swift/FileDwn/FileVC.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class FileVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    //var progressView: UIProgressView!
+    
+    var completionHandler: AWSS3TransferUtilityDownloadCompletionHandlerBlock?
+
+    
     override func viewDidLoad() {
         print("Hello")
         super.viewDidLoad()
+
         
-        
-        
-        
+
         let downloadFilePath = "/Users/advi/Desktop/Nosh-Swift/Dwnloads/my.json"
         
         let downloadingFileURL = NSURL.fileURLWithPath(downloadFilePath)
@@ -43,27 +52,88 @@ class FileVC: UIViewController {
                 let mytext = try NSString(contentsOfFile: downloadFilePath, encoding: NSUTF8StringEncoding)
                 print(mytext)
                 }
-                 catch {print("wtf")/* error handling here */}
+                 catch {print("error")/* error handling here */}
             }
             return nil
+
         }
         // Do any additional setup after loading the view.
-    }
+        
+        
+        // Downloading Image files
+        
+        start()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func start(){
+        
+        let S3BucketName: String = "nosh-swift"
+        let S3DownloadKeyName: String = "Bacon Buffalo Ranch McCh.png"
+        
+        let downloadFilePath = "/Users/advi/Desktop/Nosh-Swift/Dwnloads/myranch.png"
+        
+        let downloadingFileURL = NSURL.fileURLWithPath(downloadFilePath)
+        
+        
+        let expression = AWSS3TransferUtilityDownloadExpression()
+        expression.downloadProgress = {(task: AWSS3TransferUtilityTask, bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) in
+            dispatch_async(dispatch_get_main_queue(), {
+                let progress = Float(totalBytesSent) / Float(totalBytesExpectedToSend)
+                self.progressView.progress = progress
+                self.statusLabel.text = "Downloading..."
+                NSLog("Progress is: %f",progress)
+                
+            })
+        }
+        
+        self.completionHandler = { (task, location, data, error) -> Void in
+            dispatch_async(dispatch_get_main_queue(), {
+                if ((error) != nil){
+                    NSLog("Failed with error")
+                    NSLog("Error: %@",error!);
+                    self.statusLabel.text = "Failed"
+                }
+                else if(self.progressView.progress != 1.0) {
+                    self.statusLabel.text = "Failed"
+                    NSLog("Error: Failed - Likely due to invalid region / filename")
+                }
+                else if ((data) != nil){
+                    self.statusLabel.text = "Success"
+                    self.imageView.image = UIImage(data: data!)
+                }
+                else{
+                    print(data)
+                }
+            })
+        }
+        
+        let transferUtility = AWSS3TransferUtility.defaultS3TransferUtility()
+        
+        transferUtility.downloadToURL(downloadingFileURL, bucket: S3BucketName, key: S3DownloadKeyName, expression: expression, completionHander: completionHandler).continueWithBlock { (task) -> AnyObject! in
+            if let error = task.error {
+                NSLog("Error: %@",error.localizedDescription);
+                self.statusLabel.text = "Failed"
+            }
+            if let exception = task.exception {
+                NSLog("Exception: %@",exception.description);
+                self.statusLabel.text = "Failed"
+            }
+            if let _ = task.result {
+                self.statusLabel.text = "Starting Download"
+                NSLog("Download Starting!")
+                // Do something with uploadTask.
+            }
+            return nil;
+        }
     }
-    */
 
 }
+
+
